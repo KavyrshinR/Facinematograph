3520db5646f87f70d6e14a5d40108ad2d3c5b958
Roman <rom.kawirshin@gmail.com>
2018-04-15 11:17:45 +0300

    Добавлена SearchFilmsActivity и разметка

---
+++ app/src/main/java/ru/kavyrshin/facinematograph/ui/SearchFilmsActivity.kt
@@ -0,0 +1,15 @@
+package ru.kavyrshin.facinematograph.ui
+
+import android.os.Bundle
+import ru.kavyrshin.facinematograph.R
+
+class SearchFilmsActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_films)
+
+
+    }
+
+}
