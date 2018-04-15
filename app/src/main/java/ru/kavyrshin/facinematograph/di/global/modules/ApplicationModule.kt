285a8f821b4aeee04f2adef3841e4996d0771020
Roman <rom.kawirshin@gmail.com>
2018-04-15 11:17:07 +0300

    Добавлен provide context в ApplicationModule

--- app/src/main/java/ru/kavyrshin/facinematograph/di/global/modules/ApplicationModule.kt
+++ app/src/main/java/ru/kavyrshin/facinematograph/di/global/modules/ApplicationModule.kt
@@ -1,6 +1,15 @@
 package ru.kavyrshin.facinematograph.di.global.modules
 
+import android.content.Context
 import dagger.Module
+import dagger.Provides
+import ru.kavyrshin.facinematograph.FacinematographApplication
 
 @Module
-class ApplicationModule
+class ApplicationModule {
+
+    @Provides
+    fun context(application : FacinematographApplication) : Context {
+        return application
+    }
+}
