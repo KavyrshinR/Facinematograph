09e910d97906b4660cf43e4e6971b5a7c1143065
Roman <rom.kawirshin@gmail.com>
2018-04-16 10:13:53 +0300

    Добавлен переход на SearchFilmsActivity

--- app/src/main/java/ru/kavyrshin/facinematograph/ui/FavouriteFilmsActivity.kt
+++ app/src/main/java/ru/kavyrshin/facinematograph/ui/FavouriteFilmsActivity.kt
@@ -1,16 +1,26 @@
 package ru.kavyrshin.facinematograph.ui
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import ru.kavyrshin.facinematograph.R
 
-class FavouriteFilmsActivity : BaseActivity() {
+class FavouriteFilmsActivity : BaseActivity(), View.OnClickListener {
+
+    private var btnSearch: Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_favourite_films)
 
-
+        btnSearch = findViewById(R.id.btnSearch) as? Button
+        btnSearch?.setOnClickListener(this)
     }
 
+
+    override fun onClick(v: View?) {
+        startActivity(Intent(this, SearchFilmsActivity::class.java))
+    }
 
 }
