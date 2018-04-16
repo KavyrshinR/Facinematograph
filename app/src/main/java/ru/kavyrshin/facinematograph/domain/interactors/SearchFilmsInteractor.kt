a001e382a161edf4583b95d3496232d86be6a479
Roman <rom.kawirshin@gmail.com>
2018-04-16 10:05:47 +0300

    Добавлен inject в SearchFilmsInteractor

--- app/src/main/java/ru/kavyrshin/facinematograph/domain/interactors/SearchFilmsInteractor.kt
+++ app/src/main/java/ru/kavyrshin/facinematograph/domain/interactors/SearchFilmsInteractor.kt
@@ -1,12 +1,15 @@
 package ru.kavyrshin.facinematograph.domain.interactors
 
 import io.reactivex.Single
+import io.reactivex.schedulers.Schedulers
 import ru.kavyrshin.facinematograph.data.repositories.FilmsRepository
 import ru.kavyrshin.facinematograph.domain.global.models.Film
+import javax.inject.Inject
 
-class SearchFilmsInteractor public constructor(val filmsRepository: FilmsRepository) {
+class SearchFilmsInteractor public @Inject constructor(val filmsRepository: FilmsRepository) {
 
     fun searchFilmsByTitle(searchRequest: String) : Single<List<Film>> {
         return filmsRepository.search(searchRequest, 1)
+                .subscribeOn(Schedulers.io())
     }
 }
