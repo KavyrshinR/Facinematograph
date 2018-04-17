1b9a5a0707e589d3d6fab58bc112ec8b3047f794
Roman <rom.kawirshin@gmail.com>
2018-04-18 01:08:07 +0300

    Класс Film адаптирован для Room

--- app/src/main/java/ru/kavyrshin/facinematograph/domain/global/models/Film.kt
+++ app/src/main/java/ru/kavyrshin/facinematograph/domain/global/models/Film.kt
@@ -1,9 +1,12 @@
 package ru.kavyrshin.facinematograph.domain.global.models
 
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity
 data class Film(
-        @SerializedName("imdbID") val id : String,
+        @PrimaryKey @SerializedName("imdbID") val id : String,
         @SerializedName("Title") val title : String,
         @SerializedName("Year") val year : Int,
         @SerializedName("Poster") val posterSrc : String
