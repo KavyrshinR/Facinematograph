e36449763965643a25c83ee035aaba348c8fb599
Roman <rom.kawirshin@gmail.com>
2018-04-18 01:10:56 +0300

    Добавлено предоставление зависимости в DatabaseModule

--- app/src/main/java/ru/kavyrshin/facinematograph/di/global/modules/DatabaseModule.kt
+++ app/src/main/java/ru/kavyrshin/facinematograph/di/global/modules/DatabaseModule.kt
@@ -1,6 +1,22 @@
 package ru.kavyrshin.facinematograph.di.global.modules
 
+import android.arch.persistence.room.Room
+import android.content.Context
 import dagger.Module
+import dagger.Provides
+import ru.kavyrshin.facinematograph.data.database.FacinematographDatabase
+import javax.inject.Singleton
 
 @Module
-class DatabaseModule
+class DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(context: Context) : FacinematographDatabase {
+        val database: FacinematographDatabase
+                = Room.databaseBuilder(context, FacinematographDatabase::class.java, "cinemabase")
+                .build()
+
+        return database
+    }
+}
