19319be057a7cd2d792aacd7cd2bb8ffb68a1ca0
Roman <rom.kawirshin@gmail.com>
2018-04-18 01:12:07 +0300

    Добавлена новая зависимость реализованы методы

--- app/src/main/java/ru/kavyrshin/facinematograph/data/repositories/FilmsRepository.kt
+++ app/src/main/java/ru/kavyrshin/facinematograph/data/repositories/FilmsRepository.kt
@@ -3,12 +3,14 @@ package ru.kavyrshin.facinematograph.data.repositories
 import io.reactivex.Completable
 import io.reactivex.Single
 import ru.kavyrshin.facinematograph.BuildConfig
+import ru.kavyrshin.facinematograph.data.database.FacinematographDatabase
 import ru.kavyrshin.facinematograph.data.network.OmdbApi
 import ru.kavyrshin.facinematograph.domain.global.models.Film
 import ru.kavyrshin.facinematograph.domain.global.repositories.IFilmsRepository
+import java.util.concurrent.Callable
 import javax.inject.Inject
 
-class FilmsRepository public @Inject constructor(val api: OmdbApi) : IFilmsRepository {
+class FilmsRepository public @Inject constructor(val api: OmdbApi, val database: FacinematographDatabase) : IFilmsRepository {
 
     override fun search(searchRequest: String, page: Int): Single<List<Film>> {
         return api.searchFilmsByTitle("", BuildConfig.API_KEY, "movie", searchRequest, page)
@@ -16,14 +18,14 @@ class FilmsRepository public @Inject constructor(val api: OmdbApi) : IFilmsRepos
     }
 
     override fun saveFilmInFavourite(film: Film): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Completable.fromCallable(Callable { database.getFilmDao().insertFilm(film) })
     }
 
     override fun deleteFilmFromFavourite(film: Film): Completable {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return Completable.fromCallable(Callable { database.getFilmDao().deleteFilm(film) })
     }
 
     override fun getFavouriteFilms(): Single<List<Film>> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return database.getFilmDao().queryAllFilms()
     }
 }
